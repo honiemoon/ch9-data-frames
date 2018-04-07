216256e88cd94c73783a5fbf83bf3576a64396c1
Honie Moon <honiemoon@Honies-MacBook-Pro-2.local>
2018-04-06 17:54:15 -0700

    change file to show Seahawks game stats

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,14 +2,14 @@
 
 # Create a vector of the number of points the Seahawks scored in the first 4 games
 # of the season (google "Seahawks" for the scores!)
-
+seahawks <- c(24, 24, 24, 7)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each of the first 4 games of the season
-
+allowed <- c(5, 4, 5, 6)
 
 # Combine your two vectors into a dataframe called `games`
-
+games <- c(seahawks, allowed)
 
 # Create a new column "diff" that is the difference in points between the teams
 # Hint: recall the syntax for assigning new elements (which in this case will be
